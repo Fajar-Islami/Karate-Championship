57a739afe21f262063206c04c5280054041843a0
alvine yoga <alvinecom2018@gmail.com>
2020-09-11 12:28:16 -0700

    tambah pdf viewer di guidence

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, Fragment } from "react";
 import { Tabel } from "../../components";
 import { MDBDataTable } from "mdbreact";
 import { data, usiadini } from "./data";
+import '../../components/Tabel/style.scss';
 
 const Categories = () => {
 	console.log(usiadini);
@@ -9,20 +10,20 @@ const Categories = () => {
 
 	const kolom = usiadini.columns;
 	const baris = usiadini.rows;
-	const kolom2 = data.columns;
-	const baris2 = data.rows;
-	
+	// const kolom2 = data.columns;
+	// const baris2 = data.rows;
+
 	// Lifecycle React Hooks
 	useEffect(() => {
 		document.title = `Categories`;
-	}, [kolom, baris], [kolom2, baris2]);
+	}, [kolom, baris]);
 
 	return (
 			<Fragment>
 				<p className="pt-3">Categories</p>
 				<hr />
 				<MDBDataTable striped bordered small data={usiadini} />;
-				<MDBDataTable striped bordered small data={data} />;
+				{/* <MDBDataTable striped bordered small data={data} />; */}
 			</Fragment>
 	);
 };
